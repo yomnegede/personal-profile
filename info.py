8607b8681c0e6419e506282ff4d2f5bbb0390cad
yomnegede <152322212+yomnegede@users.noreply.github.com>
2024-01-11 14:39:57 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/profile.jpeg"
+about_me = "I'm Yom Negede. I am a undergraduate Computer Science Student at Georgia Tech. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/yom-negede-808755216/"
+my_github_url = "https://github.com/yomnegede"
+my_email_address = "yomnegede@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2027'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1551", "ENGL 1102", "CS 1100"], 
+    "names":["Intro to CS", "Differential Calculus", "English", "Freshman Seminar"], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["I learned how to effectively use Python to code", "I learned how to do basic differential calculus. Ex. derivatives, inflection points, etc.", "I learned how to better write papers", "I learned more about being a CS major at Georgia Tech."],
+    }
+experience_data = {
+    "ECAA Tutor ": (["- Tutored kids ages 5 - 17 in Math, Science, and Amharic",
+                                                                          "- Grades increased by an average of 5.5 points", "- Made my mom proud"],"images/tutoring.jpeg"),
+    "Event Director of International Night":(["- Organized a cultural event at my highschool ",
+                                                           "- completely sold out with 500 ticket sales"],"images/inter.png"),
+
+}
+
+projects_data = {
+    "Pokemon Exploration App": "This is a user-friendly Streamlit application for Pokémon enthusiasts. It features a detailed Pokémon search, a random Pokémon generator with statistical pie charts, and an ability-based Pokémon finder. Integrating with PokeAPI, it provides real-time data, including images and stats, making it an informative and entertaining tool for discovering and learning about the diverse world of Pokémon.", "Google Search Homepage Clone": "Basic clone of the Google Search webpage in which I independently reconstructed the Google Search homepage, creating each component to ensure an exact match in both functionality and aesthetics. Manually coding all elements, including the search bar, buttons, and navigational links, without the use of templates or frameworks. Ensured a pixel-perfect design through careful styling and layout adjustments, showcasing an acute attention to detail and design accuracy.", "Personalized Profile Webpage": "Conceptualized and developed a unique personal profile webpage from scratch, while adhering to web development best practices. Employed HTML5 to structure the content, incorporating various elements such as headings, paragraphs, images, and navigation links. Utilized CSS3 for styling, ensuring a visually appealing and user-friendly experience. Utilized CSS classes and IDs for precise styling and layout control, demonstrating an understanding of CSS specificity and selector usage."
+}
+
+programming_data = {
+    "Python": 100,
+    "CSS": 100,
+    "HTML": 100,
+    "Java": 70   
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "CSS": "😈",
+    "Java": "👾",
+    "HTML": "🥸",
+}
+spoken_icons = {"Amharic": "🇪🇹",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Amharic": "Fluent",
+    "Spanish": "Intermediate",
+}
+leadership_data = {
+    "President of NHS": (["- Coordinated many meetings throughout the year that promoted good academic habits, study halls, etc."],"images/NHS.png"),
+
+}
+activity_data={
+    "NHS Tutoring": ["- Did tutoring for 4 years for young children helping them improve their grades"]
+}
